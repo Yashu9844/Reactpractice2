88187bf3435c68eae4b337bae85284bac99cb82c
Yashavanth R Siddesh <142719716+Yashu9844@users.noreply.github.com>
2025-01-08 00:00:16 +0530

    add showDropDown

--- reactpro12/src/App.jsx
+++ reactpro12/src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
      const [users , setUsers] = useState([]);
      const [inputValue, setInputValue] = useState('');
      const [filteredUsers, setFilteredUsers] = useState([]);
-
+   const [showDrop,setDrop]= useState(false)
 
      const fetchusers =async ()=>{
 
@@ -23,6 +23,10 @@ const App = () => {
           if(query.length > 1){
           const filterData = users && users.length ? users.filter(user => user.toLowerCase().indexOf(query) > -1) : [];
           setFilteredUsers(filterData);
+          setDrop(true);
+          }else{
+            setFilteredUsers([]);
+            setDrop(false);
           }
 
      }
@@ -32,8 +36,18 @@ const App = () => {
      },[]);
 
   return (
-    <div>
-        <input type='text' value={inputValue} onChange={handleChanges} />
+    <div style={{textAlign:'center',
+      
+    }}>
+        <input style={{padding:'5px'}} type='text' value={inputValue} onChange={handleChanges}  placeholder='Please type the name...'/>
+        {showDrop && 
+        <ul>
+          {filteredUsers.map((user,index) => (
+            <h5 key={index}>{user}</h5>
+          ))}
+        </ul>
+        
+        }
     </div>
   )
 }
