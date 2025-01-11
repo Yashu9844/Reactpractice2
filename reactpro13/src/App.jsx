f3f596669546df9745884db98ab8c8b2267a9fe1
Yashavanth R Siddesh <142719716+Yashu9844@users.noreply.github.com>
2025-01-11 22:40:02 +0530

    done ui

--- reactpro13/src/App.jsx
+++ reactpro13/src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(true);
+  const restartGame = () => {
+    setSquares(Array(9).fill(null));
+    setIsXNext(true);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='container'>
+      <div className="row">
+        <Square   value={} onClick={} />
+        <Square   value={} onClick={} />
+        <Square   value={} onClick={} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="row">
+      <Square   value={} onClick={} />
+        <Square   value={} onClick={} />
+        <Square   value={} onClick={} />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <div className="row">
+      <Square   value={} onClick={} />
+        <Square   value={} onClick={} />
+        <Square   value={} onClick={} />
+      </div>
+      <button className='restart-button' onClick={restartGame}>Restart Game</button>
+    </div>
   )
 }
+
+const Square = ({value,onClick })=>{
+return (
+  <button className='square' onClick={onClick} >{value}</button>
+)
+}
+
 
 export default App
