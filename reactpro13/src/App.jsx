c8f004422b88dc7f3730d71b13c49e3780f54099
Yashavanth R Siddesh <142719716+Yashu9844@users.noreply.github.com>
2025-01-18 23:54:56 +0530

    commit

--- reactpro13/src/App.jsx
+++ reactpro13/src/App.jsx
@@ -71,7 +71,7 @@ const App = () => {
         </div>
       </div>
       <button className="restart-button" onClick={restartGame}>
-        Resta
+        Restart
       </button>
     </div>
   );
