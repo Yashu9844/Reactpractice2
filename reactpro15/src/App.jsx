cb0880d7716c7632e91637fa25f157ce88cfb7d6
Yashavanth R Siddesh <142719716+Yashu9844@users.noreply.github.com>
2025-02-01 01:31:53 +0530

    another reactproi15 proj

---
+++ reactpro15/src/App.jsx
@@ -0,0 +1,117 @@
+import React from 'react'
+
+const App = () => {
+
+ const handleTop = ()=>{
+    window.scrollTo({
+      top:0,
+      left:0,
+      
+      behavior:'smooth'})
+ }
+
+ const handleBottom = ()=>{
+    window.scrollTo({
+      top:document.body.scrollHeight,
+      left:0,
+      
+      behavior:'smooth'})
+    }
+  return (
+    <div>
+      <div className="" >
+        <button onClick={handleBottom}>Scroll to Bottom</button>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <h1>hello everyone </h1>
+        <button onClick={handleTop}>Scroll to Top</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
